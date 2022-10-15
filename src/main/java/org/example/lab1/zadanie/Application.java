5e79a722564660971f39cc30cb841feba22366b2
adgut <gut.adrian.95@gmail.com>
2022-10-15 14:58:28 +0200

    update directory contents, split Application into two separate classes

---
+++ src/main/java/org/example/lab1/zadanie/Application.java
@@ -0,0 +1,9 @@
+package org.example.lab1.zadanie;
+
+public class Application {
+    public static void main(String[] args) {
+        KoktajlTruskawkowy koktajlTruskawkowy = new KoktajlTruskawkowy();
+        koktajlTruskawkowy.jedz();
+        koktajlTruskawkowy.smak();
+    }
+}
